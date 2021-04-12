28d8deb895e81af377390cbdeaf36567b212c8ad
Hoossayn <abdulhakeemhusseni@gmail.com>
2021-04-12 14:54:56 +0100

    set up manyToMany Entity and JDBC

---
+++ src/main/java/com/example/springrecipeapp/model/UnitOfMeasure.java
@@ -0,0 +1,30 @@
+package com.example.springrecipeapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String uon;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUon() {
+        return uon;
+    }
+
+    public void setUon(String uon) {
+        this.uon = uon;
+    }
+}
